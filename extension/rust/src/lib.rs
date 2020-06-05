8e7b5b1298635ec3fe43d504847bf59a0e923b03
Joshua Lockerman <josh@timescale.com>
2020-06-05 12:12:56 -0400

    Minor refactors

--- extension/rust/src/lib.rs
+++ extension/rust/src/lib.rs
@@ -6,13 +6,15 @@ use std::{
 };
 
 use ts_extend::{
+    datum::ToDatum,
     elog,
     elog::Level::Error,
-    palloc::memory_context_switch_to,
+    palloc::in_context,
     pg_fn,
     pg_sys::{
         AggCheckCallContext,
         ArrayType,
+        Datum,
         FLOAT8OID,
         FLOAT8PASSBYVAL,
         MemoryContext,
@@ -51,25 +53,20 @@ pg_fn!{
             elog!(Error, "input time less than lowest time")
         }
 
-        let old_ctx = unsafe { memory_context_switch_to(agg_ctx) };
+        unsafe {
+            in_context(agg_ctx, || {
+                let state = state.map(|s| &mut *s).unwrap_or_else(|| {
+                    let expected_deltas = ((greatest_time - lowest_time) / (step_size * USECS_PER_SEC)) + 1;
+                    let state = GapfillDeltaTransition::new(expected_deltas as _, greatest_time, window_size, step_size)
+                        .into();
+                    Box::leak(state)
+                });
 
-        let state = state.map(|s| unsafe { &mut *s }).unwrap_or_else(|| {
-            let expected_deltas = ((greatest_time - lowest_time) / (step_size * USECS_PER_SEC)) + 1;
-            let state = GapfillDeltaTransition{
-                window: VecDeque::default(),
-                deltas: Vec::with_capacity(expected_deltas as _),
-                nulls: Vec::with_capacity(expected_deltas as _),
-                current_window_max: greatest_time,
-                current_window_min: greatest_time - window_size*USECS_PER_SEC,
-                step_size: step_size*USECS_PER_SEC,
-            }.into();
-            Box::leak(state)
-        });
-        state.add_data_point(time, val);
+                state.add_data_point(time, val);
 
-        unsafe { memory_context_switch_to(old_ctx); }
-
-        Some(state as *mut _)
+                Some(state as *mut GapfillDeltaTransition)
+            })
+        }
     }
 }
 
@@ -84,19 +81,17 @@ pg_fn!{
             elog!(Error, "must call gapfill_delta_transition as an aggregate")
         }
 
-        let old_ctx = unsafe { memory_context_switch_to(agg_ctx) };
-
-        let ret = state.map(|s| unsafe { (&mut *s).to_pg_array() });
-
-        unsafe { memory_context_switch_to(old_ctx); }
-
-        ret
+        unsafe {
+            in_context(agg_ctx, || {
+                state.map(|s| (&mut *s).to_pg_array())
+            })
+        }
     }
 }
 
 struct GapfillDeltaTransition {
     window: VecDeque<(TimestampTz, f64)>,
-    deltas: Vec<f64>,
+    deltas: Vec<Datum>,
     nulls: Vec<bool>,
     current_window_max: TimestampTz,
     current_window_min: TimestampTz,
@@ -104,6 +99,18 @@ struct GapfillDeltaTransition {
 }
 
 impl GapfillDeltaTransition {
+    pub fn new(expected_deltas: usize, greatest_time: TimestampTz, window_size: Seconds, step_size: Seconds)
+    -> Self {
+        GapfillDeltaTransition{
+            window: VecDeque::default(),
+            deltas: Vec::with_capacity(expected_deltas),
+            nulls: Vec::with_capacity(expected_deltas),
+            current_window_max: greatest_time,
+            current_window_min: greatest_time - window_size*USECS_PER_SEC,
+            step_size: step_size*USECS_PER_SEC,
+        }
+    }
+
     pub fn add_data_point(&mut self, time: TimestampTz, val: f64) {
         while !self.in_current_window(time) {
             self.flush_current_window()
@@ -119,7 +126,7 @@ impl GapfillDeltaTransition {
     fn flush_current_window(&mut self) {
         match (self.window.front(), self.window.back()) {
             (Some((_, latest_val)), Some((_, earliest_val))) => {
-                self.deltas.push(latest_val - earliest_val);
+                self.deltas.push((latest_val - earliest_val).to_datum());
                 self.nulls.push(false);
             },
             // if there are 1 or fewer values in the window, store NULL
@@ -139,7 +146,7 @@ impl GapfillDeltaTransition {
         self.flush_current_window();
         unsafe {
             construct_md_array(
-                self.deltas.as_mut_ptr() as *mut _, //TODO create safe converter
+                self.deltas.as_mut_ptr(),
                 self.nulls.as_mut_ptr(),
                 1,
                 &mut (self.deltas.len() as _),
